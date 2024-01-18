5306b941ed9e396d15245e7c125902378f37d0b3
Kingkoorbin <corbain.more@lccdo.edu.ph>
2024-01-18 16:49:18 +0800

    partial updates

--- resources/views/contributions.blade.php
+++ resources/views/contributions.blade.php
@@ -1,143 +1,191 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
         html, body {
             height: 100%;
             margin: 0;
             padding: 0;
+            line-height: 1.25;
         }
+
         body {
             font-family: 'Arial', sans-serif;
             background-image: url("{{ public_path('images/pdf_bg.jpg') }}");
             background-repeat: no-repeat;
-            background-size: cover; /* or 'contain' based on your preference */
+            background-size: 100% 100%;
             background-position: center;
+            background-attachment: fixed;
         }
+
         .page-break {
             page-break-after: always;
         }
 
         .page-space {
-            margin-top:200px;
+            margin-top: 200px;
         }
+
         table {
-            margin-left: 100px;
+            margin-left: 150px;
+            border-collapse: collapse;
+            width: 80%; /* Adjusted table width */
         }
+
+        thead {
+            display: table-header-group;
+        }
+
         th {
             background-color: #f2f2f2;
-=
-            padding: 6px;
-            font-size: 10px;
+            position: sticky;
+            top: 0;
+            padding: 10px;
+            font-size: 30px; /* Adjusted font size */
         }
-        th,
-        td {
+
+        th, td {
             text-align: left;
-            font-size: 12px;
+            font-size: 25px;
+            padding: 10px;
         }
+
         .hidden {
             opacity: 0;
             visibility: 0;
         }
+
         th.sbr-date {
             width: 100px;
         }
+
         th.sbr-no {
             width: 120px;
         }
+
         th.year {
             width: 70px;
         }
+
         th.month {
             width: 100px;
         }
+
         th.ec {
             width: 50px;
         }
+
         th.total {
             width: 100px;
         }
+
         th.ss {
             width: 100px;
         }
+
         .info {
             margin-top: 50px;
-            margin-left: 100px;
+            margin-left: 200px;
             margin-bottom: 20px;
         }
 
         .title {
-            margin-top: 170px;
+            margin-top: 250px;
             margin-left: 100px;
             margin-bottom: 20px;
         }
-        .dashed-line {
-            border-top: 1px dashed #ddd; /* Change the color to your desired color */
-            width: 100%; /* Adjust the width as needed */
+
+        .page-number {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        .next-page {
+            text-align: center;
             margin-top: 20px;
             margin-bottom: 20px;
         }
-</style>
+    </style>
 </head>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <body>
     <div class="title">
         <center>
-                <h4 style='padding: 0px; margin: 0px'>CERTIFICATE OF SSS PREMIUM PAYMENTS</h4>
+            <h4 style='padding: 0px; margin: 0px'>CERTIFICATE OF SSS PREMIUM PAYMENTS</h4>
         </center>
     </div>
     <div class="info">
+        @if ($displayName)
+            <h5 style="padding: 0px; margin: 0px;">Name : {{ $displayName }}</h5>
+        @endif
 
         @if ($displaySSSNo)
-        <h5 style="padding: 0px; margin: 0px;">SSS No : {{ $displaySSSNo }}</h5>
+            <h5 style="padding: 0px; margin: 0px;">SSS No : {{ $displaySSSNo }}</h5>
         @endif
 
-        @if ($displayName)
-        <h5 style="padding: 0px; margin: 0px;">Name : {{ $displayName }}</h5>
-        @endif
-
-        @if ($displayCoverage)
-        <h5 style="padding: 0px; margin: 0px;">Coverage: {{ $displayCoverage }}</h5>
+        @if ($contributions->count() > 0)
+            @php
+                $minDate = Carbon\Carbon::parse($contributions->min('batchDate'));
+                $maxDate = Carbon\Carbon::parse($contributions->max('batchDate'));
+                $minYear = $minDate->format('Y');
+                $maxYear = $maxDate->format('Y');
+                $minMonth = $minDate->format('F');
+                $maxMonth = $maxDate->format('F');
+            @endphp
+            <h5 style="padding: 0px; margin: 0px;">Coverage: {{ $minMonth . ' ' . $minYear }} to {{ $maxMonth . ' ' . $maxYear }}</h5>
         @endif
     </div>
-    @php
-        $currentYear = null;
-    @endphp
-    @foreach($contributions as $index => $contribution)
-        @if($index > 0 && $index % 10 === 0)
-            <div class="page-break"></div>
-            <div class="page-space"></div>
-        @endif
-        <table>
-            <thead>
-                <tr>
-                    <th class="{{ $index !== 0 ? 'hidden year' : 'year' }}">Year</th>
-                    <th class="{{ $index !== 0 ? 'hidden month' : 'month' }}">Month</th>
-                    <th class="{{ $index !== 0 ? 'hidden sbr-date' : 'sbr-date' }}">SBR Date</th>
-                    <th class="{{ $index !== 0 ? 'hidden sbr-no' : 'sbr-no' }}">SBR No</th>
-                    <th class="{{ $index !== 0 ? 'hidden ss' : 'ss' }}">SS</th>
-                    <th class="{{ $index !== 0 ? 'hidden ec' : 'ec' }}">EC</th>
-                    <!-- Add other columns as needed -->
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td class="{{ $currentYear === substr($contribution['batchDate'], 0, 4) ? 'hidden' : '' }}">{{ substr($contribution['batchDate'], 0, 4) }}</td>
-                    <td>{{ Carbon\Carbon::parse($contribution['batchDate'])->format('F') }}</td>
-                    <td>{{ $contribution['sbr_date'] }}</td>
-                    <td>{{ $contribution['sbr_no'] }}</td>
-                    <td>{{ $contribution['ss'] }}</td>
-                    <td>{{ $contribution['ec'] }}</td>
 
-                    <!-- Add other columns as needed -->
-                </tr>
+    <table>
+        <thead>
+            <tr>
+                <th style="text-align: center;" class="year">Year</th>
+                <th style="text-align: center;" class="month">Month</th>
+                <th style="text-align: center;" class="ss">SS</th>
+                <th style="text-align: center;" class="ec">EC</th>
+                <th style="text-align: center;" class="sbr-no">SBR No</th>
+                <th style="text-align: center;" class="sbr-date">SBR Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            @php
+                $currentYear = null;
+            @endphp
+            @foreach($contributions as $index => $contribution)
+                @if($index > 0 && $index % 24 === 0)
             </tbody>
-        </table>
-        @php
-            $currentYear = substr($contribution['batchDate'], 0, 4);
-        @endphp
-    @endforeach
-
-    <div class="dashed-line"></div>
+                </table>
+                <div class="page-break"></div>
+                <div class="page-space"></div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th style="text-align: center;" class="year">Year</th>
+                            <th style="text-align: center;" class="month">Month</th>
+                            <th style="text-align: center;" class="ss">SS</th>
+                            <th style="text-align: center;" class="ec">EC</th>
+                            <th style="text-align: center;" class="sbr-no">SBR No</th>
+                            <th style="text-align: center;" class="sbr-date">SBR Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                @endif
+                <tr style="margin-bottom: 10px;">
+                    <td style="text-align: center;" class="{{ $currentYear === substr($contribution['batchDate'], 0, 4) ? 'hidden' : '' }}">{{ substr($contribution['batchDate'], 0, 4) }}</td>
+                    <td>{{ Carbon\Carbon::parse($contribution['batchDate'])->format('F') }}</td>
+                    <td style="text-align: right;">@if($index % 24 === 0) Php @endif{{ $contribution['ss'] }}</td>
+                    <td style="text-align: right;">@if($index % 24 === 0) Php @endif{{ $contribution['ec'] }}</td>
+                    <td style="text-align: center;">{{ $contribution['sbr_no'] }}</td>
+                    <td style="text-align: center;">{{ Carbon\Carbon::parse($contribution['sbr_date'])->format('m-d-Y') }}</td>
+                </tr>
+                @php
+                    $currentYear = substr($contribution['batchDate'], 0, 4);
+                @endphp
+            @endforeach
+        </tbody>
+    </table>
     <center>=== Nothing Follows === </center>
     <br/>
     <br/>
@@ -147,17 +195,15 @@
     </center>
     <center>
         <div style=' margin-top: 30px; '>
-            <h6 style='padding: 0px; margin: 0px'>Prepared By</h6>
-            <p style='padding: 0px; margin: 0px'>MS. CRISTIE LYN. ABAMONGA</h5>
-            <p style='padding: 0px; margin: 0px; font-size: 12px;'>Payroll Incharge</h5>
+            <h6 style='padding: 0px; margin-bottom: 40px;'>Prepared By</h6>
+            <p style='padding: 0px; margin: 0px'>MS. CRISTIE LYN. ABAMONGA</p>
+            <p style='padding: 0px; margin: 0px; font-size: 30px;'>Payroll Incharge</p>
         </div>
         <div style=' margin-top: 30px; '>
-            <h6 style='padding: 0px; margin: 0px'>Noted By</h6>
-            <p style='padding: 0px; margin: 0px'>S. MARIA ADEILA D. SERING, RVM</h5>
-            <p style='padding: 0px; margin: 0px; font-size: 12px;'>Vice President for Finance</h5>
+            <h6 style='padding: 0px; margin-bottom: 40px;'>Noted By</h6>
+            <p style='padding: 0px; margin: 0px'>S. MARIA MAE M. ANACAYA, RVM</p>
+            <p style='padding: 0px; margin: 0px; font-size: 30px;'>Vice President for Finance</p>
         </div>
     </center>
 </body>
 </html>
-
-
